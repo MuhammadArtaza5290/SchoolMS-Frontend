4369ab8ae76191da6090be189f8a3035c821d8c2
MuhammadArtaza5290 <hafizartaza098@gmail.com>
2025-08-27 16:21:45 -0700

    3rd commit

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://school-ms-backend.vercel.app',
   withCredentials: true
 });
 
