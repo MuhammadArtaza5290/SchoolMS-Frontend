41bd18a931179e94013107660c3808fdd8d7b5e5
MuhammadArtaza5290 <hafizartaza098@gmail.com>
2025-08-27 20:04:41 -0700

    4rt commit

--- src/pages/admin-profile/AdminProfilee.jsx
+++ src/pages/admin-profile/AdminProfilee.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import './adminProfile.css'
 import axios from '../../config/axiosConfig'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
@@ -10,34 +10,32 @@ import Footer from '../../components/footer/Footer'
 
 function AdminProfilee() {
   const location = useLocation();
-  let[user, setUser] = useState('')
-  // const navigate = useNavigate();
+  let[user, setUser] = useState(null)
+  const navigate = useNavigate();
     useEffect(()=>{
        axios.get('/adminprofile')
       .then(res=>setUser(res.data))
       .catch((error)=>{
-        console.log(error);
-        
-        // const role = error.response.data.role;
-        // if (error.response.status === 403 && role === 'admin') {
-        //   navigate('/adminProfile')
-        // }else if(error.response.status === 403 && role === 'teacher'){
-        //   setUser('');
-        //   navigate('/teacherProfile')
-        // }else if(error.response.status === 403 && role === 'student'){
-        //   setUser('');
-        //   navigate('/studentProfile')
-        // }else{
-        //   setUser('')
-        //   navigate('/')
-        // }
+        const role = error.response.data.role;
+        if (error.response.status === 403 && role === 'admin') {
+          navigate('/adminProfile')
+        }else if(error.response.status === 403 && role === 'teacher'){
+          setUser(null);
+          navigate('/teacherProfile')
+        }else if(error.response.status === 403 && role === 'student'){
+          setUser(null);
+          navigate('/studentProfile')
+        }else{
+          setUser(null)
+          navigate('/')
+        }
       })
     }, [location.pathname])  // line no 7,10,17 ko hum use krta han jab hum ny useEffect ko path ki location change hony pr rerender krana hota ha..
    const isMainPage = location.pathname === '/adminProfile'
   return (
     <>
     <div className="admin-container">
-      <Navbar role={user.role} image={`${user.image}`} name={user.name} email={user.email}/>
+      <Navbar role={user?.role} image={`${user?.image}`} name={user?.name} email={user?.email}/>
        <div className="page-content">
         {isMainPage && (
           <div style={{width: '100%', height: '100vh'}}>
